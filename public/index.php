96222cab1ed64219b65a1389700d8f67602e23b0
laurent-offi <laurent.courcelle.lh@gmail.com>
2021-02-08 16:56:31 +0100

    Création dus site web

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once("../controllers/boot_controller.php");
+
+    include("../views/header_views.php");
+
+
+
+
+?>
---
+++ views/header_views.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>    
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="navbar-left"><a href="#">Left 1</a></li>
+          <li class="navbar-left"><a href="#">Left 2</a></li>
+          <li class="active"><a href="#">Center 1</a></li>
+          <li><a href="#">Center 2</a></li>
+          <li><a href="#">Center 3</a></li>
+          <li class="navbar-right"><a href="#">Right 2</a></li>
+          <li class="navbar-right"><a href="#">Right 1</a></li>
+        </ul>
+      </div>
+    </nav>
+    <h1>Hello</h1>
+</div>
