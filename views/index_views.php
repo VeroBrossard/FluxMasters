c010e21e71e9edfad74104647a33495bdfd14579
Véro Brossard <brossard.vero.pro@laposte.net>
2021-02-10 11:56:32 +0100

    avancée sur affichage items et modale

--- views/index_views.php
+++ views/index_views.php
@@ -1,17 +1,19 @@
     <body>
         <!-- pour memoire  logo 01.net :
         src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-1/cp0/p50x50/75435858_10158004797767650_4207896055368384512_n.jpg?_nc_cat=1&ccb=2&_nc_sid=dbb9e7&_nc_ohc=xx80r1C07HMAX96DW6W&_nc_ht=scontent-cdt1-1.xx&tp=27&oh=66c49190dafeb285c0aca3ee62c43908&oe=6046FE1F" -->
-
+        <h1 style="color:white; text-align:center;"><?= $fluxTitle?> </h1>
         <?php
         // pour   $cardsInfos[$countTotalItems] = ['title' => $title, 'date' => $pubdate, 'urlImg' => $urlImg, 'description' => $description, 'link' => $linkArticle];
-        $sous_util = $cardsInfos[1];  // exemple recup infos du 1éme item (!! le compte commence à 1)
+        //$sous_util = $cardsInfos[1];  // exemple recup infos du 1éme item (!! le compte commence à 1)
         // echo '$sous_util / urlimg  =  ' . $sous_util['urlImg'] . '<br><br>';
+        foreach ($cardsInfos as $key => $infos) {
+            $sous_util = $cardsInfos[$key];
         ?>
         <!-- <div class="containt" style="border: 2px solid gold;">
             <div class="rss-card shadow p-3" style="border: 2px solid red;">
                 <div class="row" style="border: 2px solid black;">
                     <div class="col-md-6" style="border: 2px solid pink;">
-                        <img style="border: 2px solid blue; float: left;" width="50px" src="<?= $sous_util['urlImg'] ?>">
+                        <img style="border: 2px solid blue; float: left;" width="50px" src="<?= $sous_util['urlImg'] ?>"> article du <?= $sous_util['date'] ?>
                         <span><?= $sous_util['title'] ?></span>
                     </div>
 
@@ -23,6 +25,7 @@
         </div>  -->
 
         <div id="Exo_4" class="container p-0 my-0 bg-info text-dark border ">
+        
             <div class="col-6 text-wrap p-0"> <br />
                 <div class="card ">
                     <div class="row text-dark">
@@ -30,7 +33,7 @@
                             <img src="<?= $sous_util['urlImg'] ?>" style=" border: 2px solid blue; float: left;" width="100px" alt="mini photo de l'article">
                             <h5 class="card-title"><?= $sous_util['title'] ?></h5>
                             <!-- <p class="card-text">L'école des métiers du numérique</p> -->
-
+                            article du <?= $sous_util['date'] ?>
                             <button type="button" data-bs-toggle="modal" data-bs-target="#infos" class="btn btn-primary ">+
                                 d'info</button>
 
@@ -64,6 +67,9 @@
                 </div>
             </div>
         </div> <!-- fin du modal fade   -->
+        <?php
+        }
+        ?>
     </body>
 
     </html>
